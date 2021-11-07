4a65d1a830822d6e3d6f359eac1718634328196f
Adrian <adriantutuianu01@gmail.com>
2021-11-07 23:27:03 +0000

    Functionality to get a quote fixes #5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import "./App.css";
 
 function App() {
   const year = new Date().getFullYear();
+  const getQuote = async (path) => {
+    try {
+      await fetch(path)
+        .then((result) => result.json())
+        .then((result) => {
+          console.log(result);
+        });
+    } catch (error) {
+      console.log("Failed to retrieve the quote: " + error);
+    }
+  };
+
+  const endpoint = "https://api.quotable.io/random";
+
+  getQuote(endpoint);
 
   return (
     <div className="App">
