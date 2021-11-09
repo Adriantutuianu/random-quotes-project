0e94793ff213aaeaf6bdc21343ff005d4cbb4881
Adrian <adriantutuianu01@gmail.com>
2021-11-09 11:52:23 +0000

    Loading text done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [quote, setQuote] = useState({});
+  const [loading, setLoading] = useState(false);
+
   const year = new Date().getFullYear();
 
   useEffect(() => {
@@ -11,6 +13,7 @@ function App() {
   }, []);
 
   const getQuote = async (path) => {
+    setLoading(true);
     try {
       await fetch(path)
         .then((result) => result.json())
@@ -20,6 +23,7 @@ function App() {
     } catch (error) {
       console.log("Failed to retrieve the quote: " + error);
     }
+    setLoading(false);
   };
 
   const endpoint = "https://api.quotable.io/random";
@@ -40,15 +44,22 @@ function App() {
     <div className="App">
       <header>Random Quotes</header>
       <main>
-        <div className="quote-container">
-          <button className="new-quote-btn" onClick={() => getQuote(endpoint)}>
-            New Quote
-          </button>
-          <ul className="tags">{tags}</ul>
-          <div className="quote">
-            {quote.content} - {quote.author}
+        {loading ? (
+          "Loading, please wait..."
+        ) : (
+          <div className="quote-container">
+            <button
+              className="new-quote-btn"
+              onClick={() => getQuote(endpoint)}
+            >
+              New Quote
+            </button>
+            <ul className="tags">{tags}</ul>
+            <div className="quote">
+              {quote.content} - {quote.author}
+            </div>
           </div>
-        </div>
+        )}
       </main>
 
       <footer>@{year} - Made by Adrian Tut.</footer>
